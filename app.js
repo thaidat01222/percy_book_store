749a2edd17e3e7673c6637677b76e2bf2a8531fa
thaidat01222 <thaidat01222@gmail.com>
2021-05-27 09:49:10 +0700

    login

--- app.js
+++ app.js
@@ -5,8 +5,18 @@ const bodyParser = require('body-parser');
 const fileUpload = require('express-fileupload');
 const path = require('path');
 const session = require('express-session');
+const e = require('express');
+const { count } = require('console');
 
-
+const app = express();
+const port = 4000;
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json());
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 
 const db = mysql.createConnection({
@@ -23,35 +33,51 @@ db.connect((err) => {
 });
 global.db = db;
 
-const app = express();
-const port = 4000;
-
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/api/trending', (req, res) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
     var sql = "SELECT * FROM book";
     db.query(sql, (err, results) => {
         if (err) {
             throw err;
         }
         res.json({ booksending: results })
-        console.log(results)
     });
 });
 
 app.get('/api/lastestupdate', (req, res) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
     var sql = "SELECT nameofbook FROM book";
     db.query(sql, (err, results) => {
         if (err) {
             throw err;
         }
         res.json({ booksending: results })
-        console.log(results)
     });
 });
+
+
+app.post('/login', function (req, res) {
+    console.log("da nhan req");
+    var user_name = req.body.user;
+    var password = req.body.password;
+    console.log(user_name + " " + password);
+    var sql = "SELECT * FROM `user`"
+    db.query(sql, (err, results) => {
+        if (err) {
+            throw err;
+        }
+        var match = false;
+        for (i = 0; i < results.length; i++) {
+            if ((user_name == results[i].username) && (password == results[i].password)) {
+                console.log(results[i])
+                res.status(200).json(results[i]);
+                match = true;
+            }
+        }
+        if (match == false) {
+            console.log("sai thong tin")
+            res.status(400).send;
+        }
+    })
+})
 
 app.listen(port, () => console.log(`listening on port ${port}`));
--- my-react-app/src/components/Login.js
+++ my-react-app/src/components/Login.js
@@ -1,28 +1,69 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, BrowserRouter as Router, Route,Link } from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
+import axios from 'axios'
 class Login extends Component {
     constructor(props) {
         super(props);
+        this.handleUserChange = this.handleUserChange.bind(this);
+        this.handlePasswordChange = this.handlePasswordChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
         this.state = {
-            Username: '',
-            Password: ''
+            user: '',
+            password: '',
+            redirect: false
         };
     }
+    handleUserChange(e) {
+        this.setState({ user: e.target.value })
+    }
+    handlePasswordChange(e) {
+        this.setState({ password: e.target.value })
+    }
+    handleSubmit(e) {
+        e.preventDefault();
+        const account = {
+            user: this.state.user,
+            password: this.state.password
+        }
+        console.log("data sent: " + JSON.stringify(account));
+        axios.post('http://localhost:4000/login', account)
+            .then(response => {
+                if (response.status == 200) {
+                    console.log("da login");
+                    this.setState({ redirect: true });
+                }
+                console.log(response);
+                if (response.status == 400) {
+                    alert("Sai Thong Tin Tai Khoan");
+                    console.log("sai thong tin");
+                }
+            })
+            .catch(error => {
+                console.log(error);
+            });
+    }
     render() {
+        if (this.state.redirect) {
+            console.log("chuyen trang");
+            alert("Welcome Back!")
+            return (
+                <Redirect to="/" />
+            )
+        }
         return (
-                    <div className="login">
-                        <div className="login-form">
-                            <h1 className="login-text">ĐĂNG NHẬP</h1>
-                            <input type="username" id="user-input" placeholder="Tên người dùng hoặc địa chỉ email" required></input>
-                            <input type="password" id="password-input" placeholder="Mật khẩu" required></input>
-                            <button className="button-login">Đăng Nhập</button>
-                        </div>
-                        <div className="sign-up-in-here">
-                            <Link to="/signup" className="sign-up-link">Đăng Ký Nè</Link>
-                        </div>
-                    </div>
-            
+            <div className="login">
+                <div className="login-form">
+                    <h1 className="login-text">ĐĂNG NHẬP</h1>
+                    <input type="username" onChange={this.handleUserChange} id="user-input" placeholder="Tên người dùng hoặc địa chỉ email" required></input>
+                    <input type="password" onChange={this.handlePasswordChange} id="password-input" placeholder="Mật khẩu" required></input>
+                    <button type="submit" className="button-login" onClick={this.handleSubmit} >Đăng Nhập</button>
+                </div>
+                <div className="sign-up-in-here">
+                    <Link to="/signup" className="sign-up-link">Đăng Ký</Link>
+                </div>
+            </div>
+
 
         )
 
